6e542d42b36007d566fa07a7cbf516854c355c5d
nm2501 <devnull@localhost>
2013-03-15 14:48:19 +0000

    Added a runPriorToBuild to the stamper plugin. If you're using a recent enough version of devenv then the task dependencies will be set up for you.

--- stamper-plugin/src/main/groovy/holygradle/stamper/StampingHandler.groovy
+++ stamper-plugin/src/main/groovy/holygradle/stamper/StampingHandler.groovy
@@ -4,19 +4,20 @@ import org.gradle.api.Project
 import org.gradle.util.ConfigureUtil
 
 class StampingHandler {
-    
     Project m_project
+    
     public def m_fileReplacers = []
     public def m_patternReplacers = []
     
     public String taskName = "stampFiles"
     public String taskDescription = "Stamp things"
-
+    public boolean runPriorToBuild = false
+    
     StampingHandler(Project project) {
         m_project = project
     }    
     
-    def files (String filePattern, Closure config) {
+    def files(String filePattern, Closure config) {
         def replacer = new Replacer(filePattern)
         ConfigureUtil.configure(config, replacer)
         m_patternReplacers.add(replacer)
