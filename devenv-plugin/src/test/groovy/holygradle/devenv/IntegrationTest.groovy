62d4f6113545a9ef4164992ae86f083d932ebe8f
nm2501 <devnull@localhost>
2013-03-21 17:22:58 +0000

    In the spirit of test first, I've added a test for support for multiple solution files in the one project. The feature isn't implemented yet so the test is commented out.

--- devenv-plugin/src/test/groovy/holygradle/devenv/IntegrationTest.groovy
+++ devenv-plugin/src/test/groovy/holygradle/devenv/IntegrationTest.groovy
@@ -19,4 +19,23 @@ class IntegrationTest extends TestBase {
         assertTrue(new File(libDir, "Release/foo_r64.lib").exists())
         assertTrue(new File(libDir, "Release/foo_r64.lib").exists())
     }
+    
+    @Test
+    public void testMultiCompiler() { 
+        def projectDir = new File(getTestDir(), "multi_compiler")
+        def libDir = new File(projectDir, "build/lib")
+        if (libDir.exists()) {
+            libDir.deleteDir()
+        }
+        // Not implemented this feature yet.
+        /*
+        invokeTask(projectDir, "buildRelease", "buildDebug")
+        assertTrue(new File(libDir, "vc10/Debug/foo_vc10_d32.lib").exists())
+        assertTrue(new File(libDir, "vc10/Debug/foo_vc10_d64.lib").exists())
+        assertTrue(new File(libDir, "vc10/Release/foo_vc10_r64.lib").exists())
+        assertTrue(new File(libDir, "vc10/Release/foo_vc10_r64.lib").exists())
+        assertTrue(new File(libDir, "vc9/Debug/foo_vc9.lib").exists())
+        assertTrue(new File(libDir, "vc9/Release/foo_vc9.lib").exists())
+        */
+    }
 }
