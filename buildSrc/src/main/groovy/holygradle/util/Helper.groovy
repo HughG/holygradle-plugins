78a96c78ad8e885469253d2de5340bcbade400d6
Hugh Greene <bithugh@tameter.org>
2013-06-20 10:08:24 +0100

    Build/release clean-ups.
    * Added some static type info.
    * Replaced the amusing but unhelpful "Beware the daemon" error message.
    * Changed the default version for new plugins to have only three digits, as
    that's our new recommended numbering scheme.
    * Changed choosePluginVersion to take advantage of the recent addition of
    project dependencies between plugins.  If we're publishing a module which
    depends on some other module that we're also publishing, use the to-be-published
    version number, instead of trying to look up the last published version.  This
    makes it easy to publish multiple plugins at once.

--- buildSrc/src/main/groovy/holygradle/util/Helper.groovy
+++ buildSrc/src/main/groovy/holygradle/util/Helper.groovy
@@ -3,6 +3,7 @@ package holygradle.util
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
 
 class Helper {
@@ -23,34 +24,53 @@ class Helper {
     }
     
     public static void choosePluginVersion(Project project, String confName, String group, String moduleName) {
-        if (project.gradle.taskGraph.hasTask(project.publishRelease)) {
-            String latestVersion = getLatestVersionNumber(project, group, moduleName)
-            if (latestVersion == null) {
-                throw new RuntimeException("${group}:${moduleName} is stated as a dependency, but it has not previously been published. Please publish that module first.")  
+        if (project.gradle.taskGraph.hasTask(project.tasks["publishRelease"])) {
+            String latestVersion
+            if (project.gradle.taskGraph.hasTask(":${moduleName}:publishRelease")) {
+                // We're also publishing the plugin we want to depend on, so ask that project for its version.
+                latestVersion = project.rootProject.project(moduleName).version
+            } else {
+                // Try to find the latest published plugin version.
+                latestVersion = getLatestVersionNumber(project, group, moduleName)
+                if (latestVersion == null) {
+                    throw new RuntimeException(
+                        "${group}:${moduleName} is stated as a dependency, " +
+                        "but it has not previously been published. Please publish that module first."
+                    )
+                }
             }
-            project.dependencies.add(confName, "${group}:${moduleName}:${latestVersion}")
+            final GString dependencyNotation = "${group}:${moduleName}:${latestVersion}"
+            project.logger.debug "Adding dependency from ${project.name} (version ${project.version}) " +
+                "on ${dependencyNotation} " +
+                "for configuration ${confName}"
+            project.dependencies.add(confName, dependencyNotation)
         } else {
             String username = System.getProperty("user.name").toLowerCase()
-            project.dependencies.add(confName, "${group}:${moduleName}:${username}-SNAPSHOT") { changing = true }
+            project.dependencies.add(confName, "${group}:${moduleName}:${username}-SNAPSHOT") { ExternalModuleDependency it ->
+                it.changing = true
+            }
         }
     }
     
     public static void chooseNextVersionNumberWithUserInput(Project project) {
-        String latestVersion = getLatestVersionNumber(project, project.group, project.name)
+        String latestVersion = getLatestVersionNumber(project, project.group.toString(), project.name.toString())
         
         Console console = System.console()
         if (console == null) {
             // Work-around for this bug: http://issues.gradle.org/browse/GRADLE-2310
             println "Null System.console(). Are you using the Gradle Daemon? If so, don't. "
             println "It prevents acquiring user input. Please use: --no-daemon"
-            throw new RuntimeException("Beware the daemon.")
+            throw new RuntimeException("You can't publish when running with the Gradle daemon, " +
+                "because we need to prompt at the console for version numbers. " +
+                "Run with --no-daemon."
+            )
         }
 
         console.println()
         console.println("Choose the version for publishing ${project.group}:${project.name}.")
-        String proposedVersion = null
+        String proposedVersion
         if (latestVersion == null) {
-            proposedVersion = "1.0.0.0"
+            proposedVersion = "1.0.0"
             println "Failed to determine the most recent version of ${project.group}:${project.name}."
             println "Maybe you can't connect to your repository, or maybe you've never published this"
             println "module before. Proposing '${proposedVersion}' as the version number."
