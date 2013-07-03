8c977d5764ed73c44c44be2c9bed021bb47ffaf8
sbrad <scotty_bradley@hotmail.com>
2013-07-03 17:08:17 +0100

    GR #2504 (relates to) - Extended test for intrepid:DefaultPublishPackagesExtension

--- intrepid-plugin/src/test/groovy/holygradle/publishing/DefaultPublishPackagesExtensionTest.groovy
+++ intrepid-plugin/src/test/groovy/holygradle/publishing/DefaultPublishPackagesExtensionTest.groovy
@@ -15,30 +15,35 @@ import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
 
 class DefaultPublishPackagesExtensionTest extends TestBase {
+    
     @Test
     public void testDependenciesInIvyXml() {
-        File projectDir = new File(getTestDir(), "projectA")
-        File publicationsDir = new File(projectDir, "build/publications")
-        
-        if (publicationsDir.exists()) {
-            publicationsDir.deleteDir()
-        }
-        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-        invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
-            launcher.forTasks("generateIvyModuleDescriptor")
-            launcher.withArguments("--info")
-        }
-        
-        File ivyXml = new File(publicationsDir, "ivy/ivy.xml")
-        assertTrue(ivyXml.exists())
-        File regTestFile = regression.getTestFile("ProjectA_IvyXml")
-        // Filter out the <info> tag which has a timestamp, therefore breaks the test
-        regTestFile.withPrintWriter { w ->
-            ivyXml.eachLine { l ->
-                if (!l.contains("publication=")) w.println(l)
+    
+        ["projectA", "projectB"].each { String testProjectDirName ->
+    
+            File projectDir = new File(getTestDir(), testProjectDirName)
+            File publicationsDir = new File(projectDir, "build/publications")
+            
+            if (publicationsDir.exists()) {
+                publicationsDir.deleteDir()
             }
+            Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+            invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
+                launcher.forTasks("generateIvyModuleDescriptor")
+                launcher.withArguments("--info")
+            }
+            
+            File ivyXml = new File(publicationsDir, "ivy/ivy.xml")
+            assertTrue(ivyXml.exists())
+            File regTestFile = regression.getTestFile("${testProjectDirName}_IvyXml")
+            // Filter out the <info> tag which has a timestamp, therefore breaks the test
+            regTestFile.withPrintWriter { w ->
+                ivyXml.eachLine { l ->
+                    if (!l.contains("publication=")) w.println(l)
+                }
+            }
+            regression.checkForRegression("${testProjectDirName}_IvyXml")
         }
-        regression.checkForRegression("ProjectA_IvyXml")
     }
-        
+
 }
