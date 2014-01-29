0a39d1d2e4d879ec3eef7c037c1268f81e8b451b
Hugh Greene <bithugh@tameter.org>
2014-01-29 09:28:34 +0000

    GR #3554 Refactoring / clean-up
    * artifactory-manager-plugin\src\main\groovy\holygradle\artifactory_manager\DeleteRequest.groovy
    ** Remove the "delete older than some Date" option, since it's never used
    (keeping "delete older than N days/weeks/months/... ago").
    ** Factor out lots of bits of the process() method into helper methods, and add
    extensive comments about the trickiest bit, the "sliding window".

--- artifactory-manager-plugin/src/main/groovy/holygradle/artifactory_manager/DeleteRequest.groovy
+++ artifactory-manager-plugin/src/main/groovy/holygradle/artifactory_manager/DeleteRequest.groovy
@@ -11,13 +11,12 @@ class DeleteRequest {
     private final String modules
     private final long minRequestIntervalInMillis
 
-    private Date deleteBeforeDate = null
     private int deleteUnitCount = 0
     private String deleteUnits = null
     private String deleteExplanation = ""
     private String keepExplanation = ""
-    private int keepCount = 0
-    private String keepInterval = null
+    private int keepIntervalCount = 0
+    private String keepIntervalUnit = null
     private List<String> versionsToKeep = []
     private Pattern versionRegex = null
 
@@ -26,19 +25,14 @@ class DeleteRequest {
         this.modules = modules
         this.minRequestIntervalInMillis = minRequestIntervalInMillis
     }
-    
-    public void olderThan(Date deleteOlder) {
-        deleteBeforeDate = deleteOlder
-        deleteExplanation = "older than ${deleteOlder}"
-    }
-    
+
     public void olderThan(int countUnits, String units) {
         deleteExplanation = "more than ${countUnits} ${units} old"
         deleteUnitCount = countUnits
         deleteUnits = units
     }
     
-    private static Date getLatestDateForSlidingWindow(Date date, String units) {
+    private static Date getEndOfLatestInterval(Date date, String units) {
         Calendar calendar = date.toCalendar().clearTime()
         calendar.lenient = true;
         
@@ -100,8 +94,8 @@ class DeleteRequest {
     
     public void keepOneBuildPer(int countUnits, String units) {
         keepExplanation = ", keeping one artifact every ${countUnits} ${units}"
-        keepCount = countUnits
-        keepInterval = units
+        keepIntervalCount = countUnits
+        keepIntervalUnit = units
     }
 
     public void dontDelete(String versionToKeep) {
@@ -126,124 +120,185 @@ class DeleteRequest {
         logger.info "  Deleting artifacts for '${moduleGroup.join(", ")}'"
         logger.info "  when they are ${deleteExplanation}${keepExplanation}."
         
-        Date cutoffDate = null
-        if (deleteBeforeDate != null) {
-            cutoffDate = deleteBeforeDate
-        } else if (deleteUnits != null) {
-            cutoffDate = subtractDate(artifactoryApi.getNow(), deleteUnitCount, deleteUnits)
-        } else {
+        if (deleteUnits == null) {
             logger.info "Not deleting any artifacts for module '${modules}' because no cut-off date was specified."
             return
         }
-        
+        Date cutoffDate = subtractDate(artifactoryApi.getNow(), deleteUnitCount, deleteUnits)
+
         // Gather deletion candidates for each module.
+        //noinspection GroovyAssignabilityCheck
+        def (Date earliestDateFound, Map<String, PathInfo> moduleDeleteCandidates) =
+            collectDeletionCandidates(artifactoryApi, moduleGroup, cutoffDate)
+
+        // If we need to keep regular releases then filter out some deletion candidates.
+        if (keepIntervalCount > 0 && keepIntervalUnit != null) {
+            filterCandidatesToKeepByInterval(earliestDateFound, cutoffDate, moduleDeleteCandidates)
+        }
+        
+        // We may have a hard-coded list of versions to keep.
+        filterExplicitVersionsToKeep(moduleDeleteCandidates)
+        
+        // Delete all candidates that remain.
+        deleteModuleVersions(artifactoryApi, moduleDeleteCandidates)
+    }
+
+
+    private List collectDeletionCandidates(ArtifactoryAPI artifactoryApi, String[] moduleGroup, Date cutoffDate) {
         Map<String, PathInfo> moduleDeleteCandidates = [:]
         Date earliestDateFound = artifactoryApi.getNow()
         logger.debug "  versionRegex is ${versionRegex}"
         moduleGroup.each { module ->
             String path = module.replace(":", "/")
             PathInfo deleteCandidate = new PathInfo(artifactoryApi, path)
-            
-            logger.debug "newest"
+
             deleteCandidate.filter { PathInfo it ->
-                // Filter out any folders which represent the newest 
-                (it.parent != null) && (it == it.parent.newestChild)
-            }
-            logger.debug "after cutoff"
-            deleteCandidate.filter { PathInfo it ->
-                // Filter out any folders that were created after our cut-off date.
+                // Keep this one if we can't make sense of the version
                 if (it.version == null) {
-                    false
-                } else {
-                    Date creationDate = it.creationDate
-                    if (creationDate.before(earliestDateFound)) {
-                        earliestDateFound = creationDate
-                    }
-                    creationDate.after(cutoffDate)
+                    return true
                 }
-            }
-            if (versionRegex != null) {
-                logger.debug "regex"
-                deleteCandidate.filter { PathInfo it ->
-                    // Filter out any folders which don't match our version regex
-                    if (it.version == null) {
-                        logger.debug "$it has no version"
-                        false
-                    } else {
-                        boolean doesNotMatchVersionRegex = !it.version.matches(versionRegex)
-                        logger.debug "versionRegex match? ${doesNotMatchVersionRegex ? 'N' : 'y'} ${it.path}"
-                        doesNotMatchVersionRegex
-                    }
+
+                // Keep track of the earliest date we've seen.
+                Date creationDate = it.creationDate
+                if (creationDate.before(earliestDateFound)) {
+                    earliestDateFound = creationDate
                 }
+
+                return (
+                    // Filter out any folders which represent the newest
+                    isNewestVersion(it) ||
+                    // Filter out any folders that were created after our cut-off date.
+                    isAfterCutoff(creationDate, cutoffDate) ||
+                    // Filter out any versions which don't match the regex (if it's non-null).
+                    !matchesVersionRegex(it)
+                )
             }
-            
+
             moduleDeleteCandidates[module] = deleteCandidate
             logger.debug "Added candidate for ${module}: ${deleteCandidate}"
         }
-        
-        // If we need to keep regular releases then filter out some deletion candidates.
-        if (keepCount > 0 && keepInterval != null) {
-            Date laterDate = getLatestDateForSlidingWindow(cutoffDate, keepInterval)
-            Date earlierDate = subtractDate(laterDate, keepCount, keepInterval)
-            
-            // Keep going until we have gone back to the earliest date previously found.
-            while (laterDate.after(earliestDateFound)) {
-                logger.debug "    Checking period ${earlierDate} to ${laterDate}..."
-                boolean anythingInRange = false
-                Map<String, List<PathInfo>> modulePathsInRange = [:]
-                moduleDeleteCandidates.each { module, deleteCandidate ->
-                    List<PathInfo> inRange = []
-                    logger.debug "        Looking at module $module"
-                    deleteCandidate.all { PathInfo it ->
-                        if (it.version != null) {
-                            Date date = it.creationDate
-                            // Note: we test "earlierDate <= date" and "laterDate > date" here because we want to
+        return [earliestDateFound, moduleDeleteCandidates]
+    }
+
+    private boolean isAfterCutoff(Date creationDate, Date cutoffDate) {
+        boolean afterCutoff = creationDate.after(cutoffDate)
+        logger.debug "after cutoff? ${afterCutoff} ${creationDate}"
+        afterCutoff
+    }
+
+    private boolean matchesVersionRegex(PathInfo it) {
+        // If we have no regex, we're not really filtering by regex
+        if (versionRegex == null) {
+            return true
+        }
+
+        boolean matches = it.version.matches(versionRegex)
+        logger.debug "versionRegex match? ${matches ? 'y' : 'N'} ${it.path}"
+        matches
+    }
+
+    private boolean isNewestVersion(PathInfo it) {
+        boolean isNewest = (it.parent != null) && (it == it.parent.newestChild)
+        logger.debug "newest? ${isNewest} ${it.path}"
+        isNewest
+    }
+
+    private void filterCandidatesToKeepByInterval(
+        Date earliestDateFound,
+        Date cutoffDate,
+        Map<String, PathInfo> moduleDeleteCandidates
+    ) {
+        /*
+            This method looks for versions to keep, according to the 'keepOneBuildPer "N", "Period"' syntax, which
+            initialises keepIntervalCount and keepIntervalUnit.  E.g., 'keepOneBuildPer "4", "days"'.
+
+             It does this by looking at a "sliding window" of time, moving back from the cutoff date until it passes
+             the earliest date of any version seen.  This window is a half-open interval: versions exactly matching the
+             start date/time of the window are considered for keeping, whereas versions matching the end are not.
+
+             The window is aligned to calendar units (by calling getEndOfLatestInterval): weeks, months, or years.  The
+             end of the interval is initially at the first calendar-aligned point after the cutoff date.  For example,
+             interval is "2 weeks", and the cutoff date is Tuesday 2014-01-28, then the following windows are
+             considered:
+
+                 (Mon 2014-01-20, Mon 2014-02-03] (the end of this window is the first Monday after the cutoff)
+                 (Mon 2014-01-06, Mon 2014-01-20]
+                 (Mon 2013-12-23, Mon 2014-01-06]
+                 ...
+
+             However, if the same cutoff date is used with an interval of "1 month", the following windows are used:
+
+                 (Mon 2013-12-31, Mon 2014-01-31] (the end of this window is the start of the next month after cutoff)
+                 (Mon 2013-11-30, Mon 2013-12-31]
+                 (Mon 2013-10-31, Mon 2013-11-30]
+                 ...
+
+            In each window, the method then builds a list of all version numbers which are shared by all modules in the
+            deletion request, and filters out deletion candidates for all except the last such version.  (This means
+            that, if there is no version common to all modules, no version will be kept.
+         */
+
+        Date laterDate = getEndOfLatestInterval(cutoffDate, keepIntervalUnit)
+        Date earlierDate = subtractDate(laterDate, keepIntervalCount, keepIntervalUnit)
+
+        // Keep going until we have gone back to the earliest date previously found.
+        while (laterDate.after(earliestDateFound)) {
+            logger.debug "    Checking period ${earlierDate} to ${laterDate}..."
+            boolean anythingInRange = false
+            Map<String, List<PathInfo>> modulePathsInRange = [:]
+            moduleDeleteCandidates.each { module, deleteCandidate ->
+                List<PathInfo> inRange = []
+                logger.debug "        Looking at module $module"
+                deleteCandidate.all { PathInfo it ->
+                    if (it.version != null) {
+                        Date date = it.creationDate
+                        // Note: we test "earlierDate <= date" and "laterDate > date" here because we want to
                             // know if "date" is in the half-open interval "[earlierDate, laterDate)".
-                            if ((earlierDate.compareTo(date) <= 0) && laterDate.after(date)) {
-                                logger.debug "            ${it.path} - ${date}"
-                                inRange.add(it)
-                                anythingInRange = true
-                            }
+                        if ((earlierDate.compareTo(date) <= 0) && laterDate.after(date)) {
+                            logger.debug "            ${it.path} - ${date}"
+                            inRange.add(it)
+                            anythingInRange = true
                         }
                     }
-                    modulePathsInRange[module] = inRange
                 }
-                
-                if (anythingInRange) {
-                    List<String> commonVersions = null
-                    modulePathsInRange.each { module, inRange ->
-                        if (inRange.size() > 0) {
-                            List<String> versions = inRange.collect { it.version }
-                            if (commonVersions == null) {
-                                commonVersions = versions
-                            } else {
-                                commonVersions = commonVersions.intersect(versions)
-                            }
+                modulePathsInRange[module] = inRange
+            }
+
+            if (anythingInRange) {
+                List<String> commonVersions = null
+                modulePathsInRange.each { module, inRange ->
+                    if (inRange.size() > 0) {
+                        List<String> versions = inRange.collect { it.version }
+                        if (commonVersions == null) {
+                            commonVersions = versions
+                        } else {
+                            commonVersions = commonVersions.intersect(versions)
                         }
                     }
-                    
-                    if (commonVersions != null && commonVersions.size() > 0) {
-                        String versionToSave = commonVersions.last()
-                        logger.debug "        Saving version: ${versionToSave}"
-                        moduleDeleteCandidates.each { module, deleteCandidate ->
-                            deleteCandidate.filter { PathInfo it ->
-                                // Filter out any versioned artifacts that we should save
-                                it.version == versionToSave
-                            }
+                }
+
+                if (commonVersions != null && commonVersions.size() > 0) {
+                    String versionToSave = commonVersions.last()
+                    logger.debug "        Saving version: ${versionToSave}"
+                    moduleDeleteCandidates.each { module, deleteCandidate ->
+                        deleteCandidate.filter { PathInfo it ->
+                            // Filter out any versioned artifacts that we should save
+                            it.version == versionToSave
                         }
-                    } else {
-                        logger.debug "      No common version found for all modules."
                     }
                 } else {
-                    logger.debug "      Nothing in range."
+                    logger.debug "      No common version found for all modules."
                 }
-                
-                laterDate = earlierDate
-                earlierDate = subtractDate(laterDate, keepCount, keepInterval)
+            } else {
+                logger.debug "      Nothing in range."
             }
+
+            laterDate = earlierDate
+            earlierDate = subtractDate(laterDate, keepIntervalCount, keepIntervalUnit)
         }
-        
-        // We may have a hard-coded list of versions to keep.
+    }
+
+    private void filterExplicitVersionsToKeep(moduleDeleteCandidates) {
         versionsToKeep.each { versionToKeep ->
             moduleDeleteCandidates.each { String module, PathInfo deleteCandidate ->
                 deleteCandidate.filter { PathInfo it ->
@@ -256,8 +311,9 @@ class DeleteRequest {
                 }
             }
         }
-        
-        // Delete all candidates that remain.
+    }
+
+    private void deleteModuleVersions(artifactoryApi, Map<String, PathInfo> moduleDeleteCandidates) {
         moduleDeleteCandidates.each { String module, PathInfo deleteCandidate ->
             deleteCandidate.all { PathInfo it ->
                 // Only delete folders that look like a versioned artifact i.e. 
