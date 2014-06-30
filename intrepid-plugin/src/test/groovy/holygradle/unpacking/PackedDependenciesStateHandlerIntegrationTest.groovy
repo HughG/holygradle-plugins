778e5f80dcdef80bc79080556813db2d7584479c
Hugh Greene <bithugh@tameter.org>
2014-06-30 16:23:44 +0100

    GR #3813, GR #4071 Integration test to demonstrate newly-found bug, that the ivy file for a module C will not be found if root projet A depends on configs c1 and c2 of B, and c2 depends on (a config of) C.  This is because the transitive search code won't visit a resolved dependency if it's seen the module before at all, even if it was via a different configuration.
    * intrepid-plugin\src\test\groovy\holygradle\testUtil\repository\holygradle.test\empty-config-lib\1.0\ivy-1.0.xml
    ** Add a new configuration which has no artifats, with a dependency on a separate
    module.
    
    * intrepid-plugin\src\test\groovy\holygradle\unpacking\PackedDependenciesStateHandlerIntegrationTest.groovy
    ** Test for multiple modules.
    
    * intrepid-plugin\src\test\groovy\holygradle\testUtil\repository\holygradle.test\another-lib\1.0\another-lib-buildScript-1.0.zip
    * intrepid-plugin\src\test\groovy\holygradle\testUtil\repository\holygradle.test\another-lib\1.0\another-lib-Debug-1.0.zip
    * intrepid-plugin\src\test\groovy\holygradle\testUtil\repository\holygradle.test\another-lib\1.0\another-lib-Release-1.0.zip
    * intrepid-plugin\src\test\groovy\holygradle\testUtil\repository\holygradle.test\another-lib\1.0\ivy-1.0.xml
    ** New dummy module to depend on.
    
    * intrepid-plugin\src\test\groovy\holygradle\unpacking\PackedDependenciesStateHandlerIntegrationTest\useModulesViaEmptyConfigs\build.gradle
    * intrepid-plugin\src\test\groovy\holygradle\unpacking\PackedDependenciesStateHandlerIntegrationTest\useModuleViaEmptyConfig\build.gradle
    ** Renamed folder.
    ** Added dependency on a second module.

--- intrepid-plugin/src/test/groovy/holygradle/unpacking/PackedDependenciesStateHandlerIntegrationTest.groovy
+++ intrepid-plugin/src/test/groovy/holygradle/unpacking/PackedDependenciesStateHandlerIntegrationTest.groovy
@@ -9,15 +9,20 @@ import static org.junit.Assert.assertTrue
 
 class PackedDependenciesStateHandlerIntegrationTest extends AbstractHolyGradleIntegrationTest {
     /**
-     * Tests that an indirect dependency will be fetched, even if it is reached only via a configuration which has no
-     * artifacts.
+     * Tests that indirect dependencies will be fetched, even if they is reached only via configurations which have no
+     * artifacts, and even if they are reached via different configurations.  (The latter check is because at one point
+     * the code only fetched such dependencies via the first destination configuration encountered from a given origin
+     * configuration.)
      */
     @Test
-    public void useModuleViaEmptyConfig() {
-        final File projectDir = new File(getTestDir(), "useModuleViaEmptyConfig")
+    public void useModulesViaEmptyConfigs() {
+        final File projectDir = new File(getTestDir(), "useModulesViaEmptyConfigs")
         final File extLibDir = new File(projectDir, "extlib")
-        if (extLibDir.exists()) {
-            assertTrue("Removed existing transitive packed dep symlink ${extLibDir}", extLibDir.delete())
+        final File anotherLibDir = new File(projectDir, "anotherlib")
+        [extLibDir, anotherLibDir].each {
+            if (it.exists()) {
+                assertTrue("Removed existing transitive packed dep symlink ${it}", it.delete())
+            }
         }
 
         invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
@@ -25,6 +30,8 @@ class PackedDependenciesStateHandlerIntegrationTest extends AbstractHolyGradleIn
             launcher.forTasks("fAD")
         }
 
-        assertTrue("Symlink to transitive packed dep has been created at ${extLibDir}", extLibDir.exists())
+        [extLibDir, anotherLibDir].each {
+            assertTrue("Symlink to transitive packed dep has been created at ${it}", it.exists())
+        }
     }
 }
