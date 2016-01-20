8ce616f0d80cf2322a18fb348279897b7426d4fe
Hugh Greene <bithugh@tameter.org>
2016-01-20 16:07:47 +0000

    GR #6585 Started adding Javadoc to main interfaces etc.

--- intrepid-plugin/src/main/groovy/holygradle/artifacts/ConfigurationSet.groovy
+++ intrepid-plugin/src/main/groovy/holygradle/artifacts/ConfigurationSet.groovy
@@ -4,6 +4,18 @@ import org.gradle.api.Named
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 
+/**
+ * A {@code ConfigurationSet} represents a collection of configurations which are related in some way, where that way
+ * is defined by a type.  Implementations of this interface and {@link ConfigurationSetType} provide ways to configure
+ * those relationships.  The relationships may be explicit {@link
+ * Configuration#extendsFrom(org.gradle.api.artifacts.Configuration...)} relationships, or implicit relationships
+ * regarding their intended use.  For example, a configuration set might contain configurations for different native
+ * platforms, or native compiler build options.
+ *
+ * This interface provides methods to get the names of the configurations in a given set instance
+ * and {@link ConfigurationSetType} provides methods to get mappings between corresponding configurations in different
+ * set instances which are based on compatible configuration set types.
+ */
 public interface ConfigurationSet extends Named {
     ConfigurationSetType getType()
     void setType(ConfigurationSetType type)
