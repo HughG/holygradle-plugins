1bb39913f957ba5395f1e58d5c3680974f448894
Hugh Greene <bithugh@tameter.org>
2017-04-06 17:05:00 +0100

    In passing, log more information when a launched process fails.

--- custom-gradle-core-plugin/src/main/groovy/holygradle/process/ExecHelper.groovy
+++ custom-gradle-core-plugin/src/main/groovy/holygradle/process/ExecHelper.groovy
@@ -1,5 +1,6 @@
 package holygradle.process
 
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.logging.Logger
 import org.gradle.process.ExecResult
 import org.gradle.process.ExecSpec
@@ -30,10 +31,20 @@ class ExecHelper {
         def shouldThrow = throwForExitValue(exitValue)
 
         if (exitValue != 0) {
-            logger.debug "Will ${shouldThrow ? '' : 'not '}throw for exitValue ${exitValue} from commandLine ${commandLine}."
-            logger.debug "Error output stream follows. >>>"
-            stderr.toString().eachLine { logger.debug it }
-            logger.debug "<<< Error output stream ends."
+            LogLevel logLevel = (shouldThrow ? LogLevel.ERROR : LogLevel.INFO)
+            logger.info "Will ${shouldThrow ? '' : 'not '}throw for exitValue ${exitValue} from commandLine ${commandLine}."
+            if (shouldThrow) {
+                logger.error "Failed with exitValue ${exitValue} from commandLine ${commandLine}."
+            }
+            [
+                "Standard": stdout,
+                "Error": stderr
+            ].each { name, stream ->
+                logger.log logLevel, "${name} output stream follows. >>>"
+                stream.toString().eachLine { logger.log logLevel, it }
+                logger.log logLevel, "<<< ${name} output stream ends."
+
+            }
 
             if (shouldThrow) {
                 execResult.assertNormalExitValue()
