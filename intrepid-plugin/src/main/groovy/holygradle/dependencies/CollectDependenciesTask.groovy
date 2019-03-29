807c916b7bbfe730b6960da5f27090cbb588c07e
Clare Sheehy Hutton <devnull@localhost>
2019-03-29 13:50:01 +0000

    Changing CollectDependenciesTask to use 'onlyIf' rather than 'doFirst' to check  for existence of output directory, as from 4.3 onwards Gradle was creating the  directory before this check was executed

--- intrepid-plugin/src/main/groovy/holygradle/dependencies/CollectDependenciesTask.groovy
+++ intrepid-plugin/src/main/groovy/holygradle/dependencies/CollectDependenciesTask.groovy
@@ -9,7 +9,9 @@ class CollectDependenciesTask extends Copy {
         }
 
         destinationDir = new File(project.rootProject.projectDir, CollectDependenciesHelper.LOCAL_ARTIFACTS_DIR_NAME)
-        doFirst {
+        // This has to be an "onlyIf" block, rather than "doFirst", because more recent versions of Gradle create the
+        // output folder for a task before the doFirst block executes.
+        onlyIf {
             logger.info("Copying dependencies to ${destinationDir}")
             if (destinationDir.exists()) {
                 throw new RuntimeException(
@@ -17,6 +19,7 @@ class CollectDependenciesTask extends Copy {
                     "because it would overwrite files for the running version of the holygradle plugins."
                 )
             }
+            return true
         }
 
         new CollectDependenciesHelper(this).configure(this)
