c41f008b2b7749de0ad2ae709f4eca6a7d8a8046
Hugh Greene <bithugh@tameter.org>
2019-06-02 16:06:47 +0100

    Copied/merged over dependencies tests.

--- intrepid-plugin/src/test/groovy/holygradle/dependencies/PackedDependenciesIntegrationTest.groovy
+++ intrepid-plugin/src/test/groovy/holygradle/dependencies/PackedDependenciesIntegrationTest.groovy
@@ -3,6 +3,7 @@ package holygradle.dependencies
 import holygradle.test.AbstractHolyGradleIntegrationTest
 import holygradle.test.RegressionFileHelper
 import holygradle.test.WrapperBuildLauncher
+import org.junit.Ignore
 import org.junit.Test
 
 class PackedDependenciesIntegrationTest extends AbstractHolyGradleIntegrationTest {
@@ -31,25 +32,14 @@ class PackedDependenciesIntegrationTest extends AbstractHolyGradleIntegrationTes
         final projectDir = new File(getTestDir(), "unpacking_modules_to_same_location")
         invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
             launcher.forTasks("fetchAllDependencies")
-            launcher.expectFailure(RegressionFileHelper.toStringWithPlatformLineBreaks(
-                """In root project 'unpacking_modules_to_same_location', location '${projectDir.absolutePath}\\extlib' is targeted by multiple dependencies/versions:
-    holygradle.test:example-framework:1.1 in configurations [bar]
-        which is from packed dependency sub/../extlib
-    holygradle.test:external-lib:1.1 in configurations [bar, foo]
-        which is from holygradle.test:external-lib:1.1
-        which is from packed dependency extlib
-
-FAILURE: Build failed with an exception.
-
-* What went wrong:
-Multiple different dependencies/versions are targeting the same locations."""
-            ))
+            launcher.expectFailure("""Multiple different dependencies/versions are targeting the same locations.""")
         }
     }
 
     @Test
-    public void testSameModuleVersionAtMultipleLocations() {
-        final projectDir = new File(getTestDir(), "unpacking_one_version_to_many_locations")
+    @Ignore
+    public void testSameModuleVersionInSameConfigurationAtMultipleLocations() {
+        final projectDir = new File(getTestDir(), "same_module_same_conf_multi_target")
         invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
             launcher.forTasks("fetchAllDependencies")
             launcher.expectFailure(RegressionFileHelper.toStringWithPlatformLineBreaks(
@@ -57,8 +47,17 @@ Multiple different dependencies/versions are targeting the same locations."""
 
 * What went wrong:
 Module version holygradle.test:external-lib:1.1 is specified by packed dependencies at both path 'sub/extlib' and """ +
-"'extlib'.  A single version can only be specified at one path.  If you need it to appear at more than one " +
-"location you can explicitly create links."
+                    "'extlib'.  A single version can only be specified at one path.  If you need it to appear at more than one " +
+                    "location you can explicitly create links."
             ))
         }
-    }}
+    }
+    @Test
+    public void testSameModuleVersionInDifferentConfigurationsAtMultipleLocations() {
+        final projectDir = new File(getTestDir(), "same_module_diff_conf_multi_target")
+        invokeGradle(projectDir) { WrapperBuildLauncher launcher ->
+            launcher.forTasks("fetchAllDependencies")
+            // This should work, because they're in different configurations.
+        }
+    }
+}
